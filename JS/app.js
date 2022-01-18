7a5a3f82b88ea1952f7f065cbd38643e7b85cbfa
Montso-Matlaletsa <montso.matlaletsa@gmail.com>
2022-01-17 21:30:08 -0800

    updating a toggle button

--- JS/app.js
+++ JS/app.js
@@ -1,7 +1,18 @@
-
+ let counter = 0;
 const toggleBtn = () =>{
 
     const content = document.getElementById('content');
+   
+    console.log(counter)
+    if(counter === 0){
 
-    content.style.display = 'inline';
+        content.style.display = 'inline';
+        counter++;
+        
+    }else{
+        counter = 0;
+        content.style.display = 'none';
+    }
+   
+
 }
